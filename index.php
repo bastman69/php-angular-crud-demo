1628a5dc57707f6788b3aba7af505056bdbafff1
Kostas Bastas <costasbastas@gmail.com>
2016-12-19 16:33:03 +0200

    added index.php basics

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+ 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+      
+    <title>Employees CRUD</title>
+     
+    <!-- include material design CSS -->
+    <link rel="stylesheet" href="lib/css/materialize/css/materialize.min.css" />
+     
+    <!-- include material design icons -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+     
+</head>
+<body>
+ 
+<!-- page content and controls will be here -->
+ 
+<!-- include jquery -->
+<script type="text/javascript" src="lib/js/jquery.min.js"></script>
+ 
+<!-- material design js -->
+<script src="lib/css/materialize/js/materialize.min.js"></script>
+ 
+<!-- include angular js -->
+<script src="lib/js/angular.min.js"></script>
+ 
+<script>
+// angular js codes will be here
+ 
+// jquery codes will be here
+</script>
+ 
+</body>
+</html>
